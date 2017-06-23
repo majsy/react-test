4d383ec5d3a5e2371dd1fee698b06937c4dc8c79
Madeleine Auvinen <madeleineauvinen@GoLauren.local>
2017-06-23 11:40:53 -0400

    add clear input func after submit

--- src/js/components/TodoForm.jsx
+++ src/js/components/TodoForm.jsx
@@ -14,11 +14,12 @@ export default class TodoForm extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.handleSubmit(this.state.val);
+        this.setState({val: ''});
     }
     render() {
         return (
             <form method="POST" action="/todo/add" onSubmit={this.handleSubmit} >
-                <input form="add-item" type="text" value={this.state.val} onChange={this.handleChange} />
+                <input form="add-item" type="text" ref="addItem" value={this.state.val} onChange={this.handleChange} />
                 <button type="submit">+</button>
             </form>
         )
